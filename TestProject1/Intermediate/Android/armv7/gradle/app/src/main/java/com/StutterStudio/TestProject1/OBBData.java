00d84a66ab750afc564510f17954cbcaf40f1d7c
Jableman19 <jableman@umich.edu>
2021-06-01 16:05:17 -0500

    Fixes for stutter buttons, updated trigger boxes for UI

--- TestProject1/Intermediate/Android/armv7/gradle/app/src/main/java/com/StutterStudio/TestProject1/OBBData.java
+++ TestProject1/Intermediate/Android/armv7/gradle/app/src/main/java/com/StutterStudio/TestProject1/OBBData.java
@@ -19,7 +19,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "1", // the version of the APK that the file was uploaded against
-52732684L // the length of the file in bytes
+52927378L // the length of the file in bytes
 )
 };
 };
--- TestProject1/Source/TestProject1/ReadWriter.cpp
+++ TestProject1/Source/TestProject1/ReadWriter.cpp
@@ -5,7 +5,7 @@
 
 FString UReadWriter::LoadFileToString(FString Filename) {
 	
-	FString directory = FPaths::ConvertRelativePathToFull("/sdcard/UE4Game/TestProject1/TestProject1/Content/Stutter_Speak_Content/Texts");
+	FString directory = FPaths::ConvertRelativePathToFull("/sdcard/UE4Game/TestProject1");
 	FString result;
 	IPlatformFile& file = FPlatformFileManager::Get().GetPlatformFile();
 
@@ -23,7 +23,7 @@ FString UReadWriter::LoadFileToString(FString Filename) {
 
 FString UReadWriter::SaveStringToFile(FString Filename, FString Data) {
 
-	FString directory = FPaths::ConvertRelativePathToFull("/sdcard/UE4Game/TestProject1/TestProject1/Content/Stutter_Speak_Content/Texts");
+	FString directory = FPaths::ConvertRelativePathToFull("/sdcard/UE4Game/TestProject1");
 	FString result;
 	IPlatformFile& file = FPlatformFileManager::Get().GetPlatformFile();
 
